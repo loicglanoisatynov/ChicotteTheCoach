30294597e64e5def66d220f77b348172626b871d
Ssnakyx <hamed.kaffa@ynov.com>
2024-06-05 18:00:39 +0200

    added background moovien

--- game.js
+++ game.js
@@ -4,8 +4,6 @@ const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-
-
 const dino = {
     x: 200,
     y: canvas.height - 70,
@@ -213,9 +211,58 @@ function getRandomBonusType() {
     return bonusTypes[types[Math.floor(Math.random() * types.length)]];
 }
 
+const backgroundImage = new Image();
+backgroundImage.src = 'image/city_background.png';
+
+const cloudImages = [
+    { image: new Image(), x: canvas.width, y: 50, speed: 2 },
+    { image: new Image(), x: canvas.width * 1.5, y: 150, speed: 1.5 },
+];
+
+cloudImages[0].image.src = 'image/cloud1.png';
+cloudImages[1].image.src = 'image/cloud2.png';
+
+const groundImage = new Image();
+groundImage.src = 'image/ground.png';
+
+let bgX = 0;
+let groundX = 0;
+
+function updateBackground() {
+    bgX -= gameSpeed;
+    if (bgX <= -canvas.width) {
+        bgX = 0;
+    }
+    ctx.drawImage(backgroundImage, bgX, 0, canvas.width, canvas.height);
+    ctx.drawImage(backgroundImage, bgX + canvas.width, 0, canvas.width, canvas.height);
+}
+
+function updateClouds() {
+    cloudImages.forEach(cloud => {
+        cloud.x -= cloud.speed;
+        if (cloud.x <= -cloud.image.width) {
+            cloud.x = canvas.width;
+        }
+        ctx.drawImage(cloud.image, cloud.x, cloud.y);
+    });
+}
+
+function updateGround() {
+    groundX -= gameSpeed;
+    if (groundX <= -canvas.width) {
+        groundX = 0;
+    }
+    ctx.drawImage(groundImage, groundX, canvas.height - groundImage.height, canvas.width, groundImage.height);
+    ctx.drawImage(groundImage, groundX + canvas.width, canvas.height - groundImage.height, canvas.width, groundImage.height);
+}
+
 function update() {
     if (gameRunning) {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+        updateBackground();
+        updateClouds();
+        updateGround();
 
         dino.y += dino.velocityY;
         if (dino.y < canvas.height - dino.height) {
@@ -341,53 +388,7 @@ function update() {
         frame++;
     }
     requestAnimationFrame(update);
-    const backgroundImage = new Image();
-    backgroundImage.src = 'image/city_background.png';
-
-    const cloudImages = [
-        { image: new Image(), x: canvas.width, y: 50, speed: 2 },
-        { image: new Image(), x: canvas.width * 1.5, y: 150, speed: 1.5 },
-    ];
-
-    cloudImages[0].image.src = 'image/cloud1.png';
-    cloudImages[1].image.src = 'image/cloud2.png';
-
-    let bgX = 0;
-
-    function updateBackground() {
-        bgX -= gameSpeed;
-        if (bgX <= -canvas.width) {
-            bgX = 0;
-        }
-        ctx.drawImage(backgroundImage, bgX, 0, canvas.width, canvas.height);
-        ctx.drawImage(backgroundImage, bgX + canvas.width, 0, canvas.width, canvas.height);
-    }
-
-    function updateClouds() {
-        cloudImages.forEach(cloud => {
-            cloud.x -= cloud.speed;
-            if (cloud.x <= -cloud.image.width) {
-                cloud.x = canvas.width;
-            }
-            ctx.drawImage(cloud.image, cloud.x, cloud.y);
-        });
-    }
-    const groundImage = new Image();
-    groundImage.src = 'image/ground.png';
-
-    let groundX = 0;
-
-    function updateGround() {
-        groundX -= gameSpeed;
-        if (groundX <= -canvas.width) {
-            groundX = 0;
-        }
-        ctx.drawImage(groundImage, groundX, canvas.height - groundImage.height, canvas.width, groundImage.height);
-        ctx.drawImage(groundImage, groundX + canvas.width, canvas.height - groundImage.height, canvas.width, groundImage.height);
-    }
-
 }
-
 
 restartButton.addEventListener('click', resetGame);
 
